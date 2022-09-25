06192e52329881455c32612fccba222ba289f772
Xandre <xandredepaula2312@gmail.com>
2022-09-24 21:43:41 -0300

    Limpando comentarios e espaços em branco

--- api/produtos/produtos.php
+++ api/produtos/produtos.php
@@ -2,7 +2,6 @@
 
 $prodList = "Produtos.txt";
 $pattern = "/s/";
-//preg_match($pattern, $prodList, $matches);
 
 if ($api == 'produtos') {
     if ($method == "GET") {
@@ -10,14 +9,11 @@ if ($api == 'produtos') {
             if ($acao == 'lista' && $param == '') {
                 $handle = fopen($prodList, "r");
                 while (!feof($handle)) {
-
-                    // pega o texto linha a linha
                     $resposta = fgets($handle,);
                     $resposta = str_ireplace(array("\r", "\n", '\r', '\n'), '', $resposta);
                     if (preg_match($pattern, $prodList)) {
                         echo "<hr>";
                         echo json_encode(["Produtos:" => $resposta]);
-                        var_dump(($resposta));
                     }
                 }
             }
