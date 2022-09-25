4387ad5430f5a8a35b823b73d790609d252d1e00
Xandre <xandredepaula2312@gmail.com>
2022-09-24 21:36:15 -0300

    Definindo regex

--- api/produtos/produtos.php
+++ api/produtos/produtos.php
@@ -1,20 +1,23 @@
 <?php
-$prodList = "Produtosregex.txt";
-//preg_match_all("([A-z]+s)+", $prodList);
+
+$prodList = "Produtos.txt";
+$pattern = "/s/";
+//preg_match($pattern, $prodList, $matches);
+
 if ($api == 'produtos') {
     if ($method == "GET") {
         if ($acao == '' && $param == '') echo json_encode(["ERRO" => 'Caminho nao encontrado']); {
             if ($acao == 'lista' && $param == '') {
                 $handle = fopen($prodList, "r");
                 while (!feof($handle)) {
+
                     // pega o texto linha a linha
                     $resposta = fgets($handle,);
-                    // imprime os conteudo do arquivo
-                    print_r($resposta);
-                    if ($resposta) {
-                        echo json_encode(["Produto: " => $resposta]);
-                    } else {
-                        echo json_encode(["dados" => 'Não existem dados para retornar']);
+                    $resposta = str_ireplace(array("\r", "\n", '\r', '\n'), '', $resposta);
+                    if (preg_match($pattern, $prodList)) {
+                        echo "<hr>";
+                        echo json_encode(["Produtos:" => $resposta]);
+                        var_dump(($resposta));
                     }
                 }
             }
